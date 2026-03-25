e1dbbce59cfef6315abf240ad15fe59176f26302
kanoko-west <kanoko.kn2508@gmail.com>
2026-03-25 15:43:53 +0900

    Tasksコントローラー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_25_031545) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_25_035502) do
   create_table "groups", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.text "memo"
+    t.datetime "due_date"
+    t.integer "source_type", default: 0
+    t.integer "status", default: 0
+    t.string "priority", default: "B"
+    t.boolean "is_today", default: true
+    t.boolean "is_routine", default: false
+    t.boolean "archived", default: false
+    t.datetime "completed_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["completed_at"], name: "index_tasks_on_completed_at"
+    t.index ["user_id", "is_today", "archived"], name: "idx_tasks_active_view"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
@@ -32,5 +51,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_25_031545) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "users"
   add_foreign_key "users", "groups"
 end
