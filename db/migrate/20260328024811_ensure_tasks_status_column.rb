85d39ebe5007073dc1d7b1d6c9211620c811e289
kanoko-west <kanoko.kn2508@gmail.com>
2026-03-28 14:12:59 +0900

    migrationファイルリセット

--- db/migrate/20260328024811_ensure_tasks_status_column.rb
+++
@@ -1,7 +0,0 @@
-class EnsureTasksStatusColumn < ActiveRecord::Migration[7.1]
-  def change
-    unless column_exists?(:tasks, :status)
-      add_column :tasks, :status, :integer, default: 0
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_28_024811) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_25_035502) do
   create_table "groups", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
