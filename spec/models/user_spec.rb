af7f85600edd98349e9306cf67744dd13d651ff2
kanoko-west <kanoko.kn2508@gmail.com>
2026-03-30 13:15:40 +0900

    Factorybotでのテストコードに修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nameが空では登録できない' do
-      user = User.new(name: '', email: 'test@example', password: 'abc123', password_confirmation: 'abc123')
+      user = FactoryBot.build(:user)
+      user.name = ''
       user.valid?
       expect(user.errors.full_messages).to include("Name can't be blank")
     end
