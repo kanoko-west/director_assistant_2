07b77c327da78a623df9454223431d65dae63355
kanoko-west <kanoko.kn2508@gmail.com>
2026-03-30 15:44:29 +0900

    タスクモデル単体テストコード作成

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :task do
+    association :user
+    title { "テストタスク" }
+    source_type { "Slack" }
+    due_date { Date.today }
+    is_routine { false }
+    is_today { false }
+    archived { false }
+    status { :todo }
+
+    # 日常業務（マスター）用のトレイト
+    trait :routine do
+      is_routine { true }
+      source_type { nil }
+      due_date { nil }
+    end
+  end
+end
