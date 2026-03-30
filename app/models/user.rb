aea431f73804f9ee4c3cd4e7aca80e65958ebd89
kanoko-west <kanoko.kn2508@gmail.com>
2026-03-30 13:08:48 +0900

    単体テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,6 @@ class User < ApplicationRecord
   has_many :tasks
 
   validates :name, presence: true
-  validates :email, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合で入力してください' }
   
   def set_group
     self.group ||= Group.first || Group.create!(name: "デフォルトグループ")
