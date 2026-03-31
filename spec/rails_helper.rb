6e8148abc6946c3402688524d454b696ad0ccf37
kanoko-west <kanoko.kn2508@gmail.com>
2026-03-31 13:17:04 +0900

    ユーザー新規登録ができる時のテストコード作成

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,4 +37,5 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include Rails.application.routes.url_helpers
 end
